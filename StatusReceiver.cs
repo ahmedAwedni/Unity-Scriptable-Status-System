700a32e886a8e21d921e6d36a6c4abfc840c0d3c
Ahmed Awedni <151574595+ahmedAwedni@users.noreply.github.com>
2026-04-12 05:33:50 +0100

    Enhance effect application and update ActiveEffect
    Added logic to refresh effect duration for non-stackable effects and modified ActiveEffect constructor to remove GameObject parameter.

--- StatusReceiver.cs
+++ StatusReceiver.cs
@@ -7,7 +7,20 @@ public class StatusReceiver : MonoBehaviour
 
     public void ApplyEffect(StatusEffect effect)
     {
-        ActiveEffect newEffect = new ActiveEffect(effect, gameObject);
+        // EXTENSION: Check if we should refresh duration instead of stacking
+        if (!effect.stackable)
+        {
+            ActiveEffect existingEffect = _activeEffects.Find(e => e.EffectData == effect);
+            if (existingEffect != null)
+            {
+                existingEffect.ResetDuration();
+                Debug.Log($"{effect.effectName} duration refreshed!");
+                return;
+            }
+        }
+
+        // Otherwise, add it as a new effect
+        ActiveEffect newEffect = new ActiveEffect(effect);
         _activeEffects.Add(newEffect);
         effect.OnApply(gameObject);
     }
@@ -17,6 +30,13 @@ public class StatusReceiver : MonoBehaviour
         for (int i = _activeEffects.Count - 1; i >= 0; i--)
         {
             _activeEffects[i].UpdateEffect(Time.deltaTime);
+
+            // Trigger Tick logic based on tickInterval
+            if (_activeEffects[i].ShouldTick())
+            {
+                _activeEffects[i].EffectData.OnTick(gameObject);
+            }
+
             if (_activeEffects[i].IsFinished)
             {
                 _activeEffects[i].EffectData.OnRemove(gameObject);
@@ -30,13 +50,37 @@ public class ActiveEffect
 {
     public StatusEffect EffectData;
     public float RemainingTime;
+    private float _tickTimer;
+
     public bool IsFinished => RemainingTime <= 0;
 
-    public ActiveEffect(StatusEffect data, GameObject target)
+    public ActiveEffect(StatusEffect data)
     {
         EffectData = data;
         RemainingTime = data.duration;
+        _tickTimer = data.tickInterval;
     }
 
-    public void UpdateEffect(float deltaTime) => RemainingTime -= deltaTime;
+    public void ResetDuration()
+    {
+        RemainingTime = EffectData.duration;
+    }
+
+    public void UpdateEffect(float deltaTime)
+    {
+        RemainingTime -= deltaTime;
+        _tickTimer -= deltaTime;
+    }
+
+    public bool ShouldTick()
+    {
+        if (EffectData.tickInterval <= 0) return false;
+
+        if (_tickTimer <= 0)
+        {
+            _tickTimer = EffectData.tickInterval;
+            return true;
+        }
+        return false;
+    }
 }
